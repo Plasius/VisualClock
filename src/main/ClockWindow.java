f92c709d8c029434e680e90e3df9a482f8a57437
Plasius <Plasius@users.noreply.github.com>
2017-09-01 20:40:38 +0300

    We got some progress!
    A simple JFrame is implemented as of now representing the passed hours with a progress bar. Based on some calendar magic.

--- src/main/ClockWindow.java
+++ src/main/ClockWindow.java
@@ -1,18 +1,55 @@
 package main;
 
+import java.awt.FlowLayout;
+import java.awt.MouseInfo;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
 
 public class ClockWindow extends JFrame{
+	/* uncomment for testing purposes, also shows usage example
 	public static void main(String[] args) {
-		JFrame frame= new ClockWindow("WOW");
-		frame.setSize(500, 500);
-		frame.setLocationRelativeTo(null);
+		JFrame frame= new ClockWindow();
 		frame.setVisible(true);
+		
+		
+	}*/
+	
+	
+	JProgressBar progressBar;
+	JLabel progressLabel;
+	
+	public ClockWindow() {
+		super();
+		setSize(25, 175);	//might change it later to ratios
+		setLocation(MouseInfo.getPointerInfo().getLocation().x-15, MouseInfo.getPointerInfo().getLocation().y-200);	//display on above mouse
+	    setUndecorated(true);	//we dont need title
+	    setLayout(new FlowLayout());	//it does the job
+	    
+	    //initializing the elements
+		progressBar = new JProgressBar(0, 24);
+		progressBar.setOrientation(JProgressBar.VERTICAL);
+		progressBar.setString("");
+		progressBar.setValue(getProgress());
+		progressBar.setStringPainted(true);
+		add(progressBar);
+		
+		progressLabel=new JLabel();
+		progressLabel.setText(getProgress()*100/24+"%");
+		add(progressLabel);
 	}
 	
-	
-	public ClockWindow(String name) {
-		super(name);
+	//returns an int between 0 and 24 representing the current hour
+	public static int getProgress() {
+		Date date = new Date();
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(date);
+		
+		return calendar.get(Calendar.HOUR_OF_DAY);
 		
 	}
 }
